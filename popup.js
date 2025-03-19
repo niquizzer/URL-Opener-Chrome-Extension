ce4757731e259ccc626b8097018babd377f56de5
Nequea <nequeacoleman@gmail.com>
2025-03-19 13:42:16 -0600

    added chrome storage

--- popup.js
+++ popup.js
@@ -1,24 +1,55 @@
 let submit = document.getElementById("submit");
+let links = document.getElementById("linkBox");
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+  chrome.storage.local.get(["savedUrls"]).then((result) => {
+
+    if(result.savedUrls) {
+
+      links.innerText = result.savedUrls;
+
+      console.log("Previous links returned");
+
+    }
   
+  })
+
+})
+
 submit.addEventListener("click", function() {
 
-  let links = document.getElementById("linkBox").value;
+  let inputText = [];
+  
+  inputText = links.value;
 
-  let urls = links.split('\n');
+  let urls = inputText.split('\n');
 
   urls.forEach((url) => {
+
     let trimmedUrl = url.trim(); // Removes extra spaces or hidden characters
+
     if (trimmedUrl) { // Ensures it's not an empty string
-      chrome.tabs.create({ url: trimmedUrl });
-    }
-  });
 
-});
+      chrome.tabs.create ({
 
-/*chrome.runtime.onInstalled.addListener(({reason}) => {
-    if (reason === 'install') {
-      chrome.tabs.create({
-        url: "onboarding.html"
+        url: trimmedUrl
+      
       });
+      
+
     }
-  });*/
+  }
+  )
+
+  chrome.storage.local.set({ savedUrls : inputText }).then(() => {
+
+    console.log("value set");
+
+  });
+}
+);
+
+
+
+  
