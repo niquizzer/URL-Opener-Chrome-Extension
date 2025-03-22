9a69a70cfc4c6afb3214a2ec119009b5caeb9171
niquizzer <nequeacoleman@gmail.com>
2025-03-21 20:08:28 -0600

    broke code again, working on adding save list fe

--- popup.js
+++ popup.js
@@ -1,21 +1,57 @@
 let submit = document.getElementById("submit");
 let links = document.getElementById("linkBox");
+let save = document.getElementById("save");
+let listName = prompt;
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-  chrome.storage.local.get(["savedUrls"]).then((result) => {
+  chrome.storage.local.get(["urlBox"]).then((result) => {
 
-    if(result.savedUrls) {
+    if(result.urlBox) {
 
-      result.savedUrls.split('\n');
+      result.urlBox.split('\n');
 
-      links.value = result.savedUrls;
+      links.value = result.urlBox;
 
     }
   });
 });
 
 submit.addEventListener("click", createTabs);
+save.addEventListener("click", saveList);
+
+function saveList () {
+
+let oldSaves;
+let saveInfo = links.value;
+
+if (saveInfo) {
+
+  listName = prompt("What would you like to name this list?");
+
+  console.log(`You named your saved list ${listName}`);
+
+  chrome.storage.local.get(["savedUrls"]).then((result) => {
+
+  oldSaves = result.savedUrls.value;
+
+  });
+
+} else return;
+
+  if (listName == oldSaves) {
+    
+    let overwrite = confirm("Overwrite exising list?");
+
+  } else if (listName == null) {
+
+    return;
+
+  }
+
+}
+
+
 
 //Create new chrome tab function
 function createTabs() {
@@ -38,9 +74,7 @@ function createTabs() {
 
   });
 
-  console.log(e);
-
-  chrome.storage.local.set({ savedUrls : e }).then(() => {
+  chrome.storage.local.set({ urlBox : e }).then(() => {
 
     console.log(`value sent to local storage: ${e}`);
 
